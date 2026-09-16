92d27c60bc07b25007897c457258d51bfd36e19a
RobLytton <roberto264craft@gmail.com>
2026-09-16 00:02:34 -0400

    Added Spi Test

--- Main.c
+++ Main.c
@@ -1,13 +1,21 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "comm/spi.h"
-int main() {
+#define CS PB2
+#define RST PB1
 
-    
+int main() {
     spiSetup();
     spiBitMode(MSBFIRST);
+    DDRB |= (1 << CS) | (1 << RST);
+    PORTB |= (1 << CS) | (1 << RST);
+
+    char i = 0;
     while(1) {
-        
+        PORTB &= ~(1 << CS);
+        spiTransfer(i++);
+        PORTB |= (1 << CS);
+        _delay_ms(100);
     }
     return 0;
 }
